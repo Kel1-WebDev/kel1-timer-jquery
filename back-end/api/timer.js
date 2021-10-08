400a20912d94177230ab17a077bac881e914412d
Hargi <hargimuhammad@gmail.com>
2021-10-08 20:55:47 +0700

    feature(Timer) : Post timer and response with id

--- back-end/api/timer.js
+++ back-end/api/timer.js
@@ -3,13 +3,13 @@ const { pool } = require('../db/connect');
 module.exports = function (app) {
     app.route('/timer')
         .post((request, response) => {
-            const { timer_name, time, state } = request.body
+            const { timer_name} = request.body
 
-            pool.query('INSERT INTO timer (timer_name, time, state) VALUES ($1, $2, $3)', [timer_name, time, state], (error, results) => {
+            pool.query('INSERT INTO timer (timer_name) VALUES ($1) RETURNING id', [timer_name], (error, results) => {
                 if (error) {
                     throw error
                 }
-                response.status(201).send(`timer tersimpan`)
+                response.status(201).send(`timer tersimpan dengan ID : ${results.rows[0].id}`)
             })
         });
 }
